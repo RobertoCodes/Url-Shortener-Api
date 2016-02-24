80128149b60ffe6dd1e4c0b9fcef19a62b08b637
Robert <robertandrewromano@gmail.com>
2016-02-24 15:49:57 -0500

    Add methods to create ShortUrl from a long url.

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -3,5 +3,15 @@ class ShortUrl > ActiveRecord::Base
   validates :short, :url, presence: true
   validates :short, uniqueness: true
 
+  def self.random_url
+    loop do
+      random_url = "http://xyz.com/" + SecureRandom.urlsafe_base64(16)
+      return random_url unless ShortUrl.exists?(:short => random_url)
+    end
+  end
+
+  def self.generate_from_url(long_url)
+    ShortUrl.new(url: long_url, short: ShortUrl.random_url)
+  end
 
 end
