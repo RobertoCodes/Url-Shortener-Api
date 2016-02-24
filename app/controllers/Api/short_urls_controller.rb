d8fd7ca3ef458876fc440bb310e4682ac3f8542c
Robert <robertandrewromano@gmail.com>
2016-02-24 16:41:43 -0500

    Create private method to handle array of urls in params. Modify create action to handle array of urls.

--- app/controllers/Api/short_urls_controller.rb
+++ app/controllers/Api/short_urls_controller.rb
@@ -3,10 +3,24 @@ class Api::ShortUrlsController < ApplicationController
   def create
     @short_url = ShortUrl.generate_from_url(params[:url])
     if @short_url.save
-      render :show_no_count
+      render :show
     else
       render json: @short_url.errors.full_messages
     end
+  end
+
+
+
+  private
+  def handle_url_array(url_array)
+    short_urls = []
+    url_array.each do |url|
+      short_url = ShortenedUrl.generate_from_url(url)
+      if short_url.save
+        short_urls.push(short_url)
+      end
+    end
+    return short_urls
   end
 
 end
